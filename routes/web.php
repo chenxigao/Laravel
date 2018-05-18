dc93930d83fe54f13736b3c24f97c70af2311335
chenxigao <819603673@qq.com>
2018-05-18 04:44:45 +0000

    Add user statuses

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::get('password/reset/{token}','Auth\ResetPasswordController@showResetForm'
 Route::post('password/reset','Auth\ResetPasswordController@reset')->name('password.update');//执行密码更新操作
 
 
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
